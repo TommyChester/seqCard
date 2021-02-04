79956da706a7d50f6afec6e3c0b643ba0ade58db
Tommy Chester <thomas.charles.chester@gmail.com>
2021-02-04 09:49:25 -0600

    add charge table with reference

---
+++ db/migrate/20210204154739_create_charges.rb
@@ -0,0 +1,9 @@
+class CreateCharges < ActiveRecord::Migration[6.1]
+  def change
+    create_table :charges do |t|
+      t.references :card, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
